7faf5bd5889319dffa76a731fd467fd54f974596
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-24 10:37:36 -0400

    copied over user model as a starting point.

---
+++ models/profile.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("Profile", {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        max: 32,
+        min: 3
+      },
+    
+    });
+  
+    return User;
+  };
+  
