a067c1389f3dbd60972cc030bfd4b19e566d4f43
Lam Nguyen <lamcnguyen89@gmail.com>
2020-07-24 05:55:57 -0400

    Fixed some formatting issues causing Travis to fail the tests

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    res.redirect("/user/login")
+    res.redirect("/user/login");
   }
 
 };
