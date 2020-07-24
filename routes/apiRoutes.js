4d0de99969582ab76eebb8766072362874625be3
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-24 13:40:59 -0400

    created post for profile.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,32 @@
-// const router = require("express").Router();
+const router = require("express").Router();
 
 // use this file to code your API routes
 
 // All routes start with: /api
+module.exports = function (app) {
 
-// Route: /api/example
-// i.e: router.get("/example", (req, res) => {
-//     //your code here
-// })
+    // Route: /api/example
+    // i.e: router.get("/example", (req, res) => {
+    //     //your code here
+    // })
 
-// module.exports = router;
+    app.post("/api/profile", function (req, res) {
+        db.Profile.create({
+            username: req.body.steamusername,
+            aboutme: req.body.aboutme,
+            topgamesPlayed: req.body.currently,
+            genres: req.body.favGenre,
+            achievements: req.body.achievements,
+            location: req.body.location
+        })
+            .then(function () {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function (err) {
+                res.status(401).json(err);
+            });
+    });
+
+    module.exports = router;
+
+}
