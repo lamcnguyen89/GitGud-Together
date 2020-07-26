e6f7995254f6a55a211caadb606f01037004321d
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-25 21:57:26 -0400

    changed model for games table.

--- models/games.js
+++ models/games.js
@@ -1,25 +1,50 @@
 
-module.exports = function (sequelize) {
+module.exports = function (sequelize, Datatypes) {
 // Creates a "Chirp" model that matches up with DB
-  var Game = sequelize.define("game", {
-    appid: Sequelize.STRING,
-    name: Sequelize.STRING,
-    release_date: Sequelize.STRING,
-    developer: Sequelize.STRING,
-    publisher: Sequelize.STRING,
-    platforms: Sequelize.STRING,
-    required_age: Sequelize.STRING,
-    categories: Sequelize.STRING,
-    genres: Sequelize.STRING,
-    positive_ratings: Sequelize.STRING,
-    negative_ratings: Sequelize.STRING,
-    owners: Sequelize.String,
-    price: Sequelize.String
-
+  var Game = sequelize.define("Game", {
+    appid: {
+      type: Datatypes.STRING,
+    },
+    name:{ 
+      type: Datatypes.STRING,
+    },
+    release_date:{
+     type: Datatypes.STRING,
+    },
+    developer:{
+      type: Datatypes.STRING,
+    },
+    publisher:{ 
+      type: Datatypes.STRING,
+    },
+    platforms:{ 
+      type: Datatypes.STRING,
+    },
+    required_age:{ 
+      type: Datatypes.STRING,
+    },
+    categories: {
+      type: Datatypes.STRING,
+    },
+    genres: {
+      type: Datatypes.STRING,
+    },
+    positive_ratings:{ 
+      type: Datatypes.STRING,
+    },
+    negative_ratings:{
+      types: Datatypes.STRING,
+    },
+    owners:{ 
+      type:Datatypes.STRING,
+    },
+    price:{
+      type: Datatypes.STRING,
+    } 
   });
 console.log(Game)
 
-    // Syncs with DB
+  // Syncs with DB
   Game.sync();
 
   // Makes the Chirp Model available for other files (will also create a table)
---
+++ testtableRequest.js
@@ -0,0 +1,7 @@
+// const express= require("express");
+// const router = express.Router();
+// const db = require ("./")
+
+const { Router } = require("express");
+
+Router.length()
