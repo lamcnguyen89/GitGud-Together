167cafa2b3f4507d3ea3666d4d8d1a3beb536c74
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-27 15:37:16 -0400

    set up update button and template for put.

--- public/js/profile.js
+++ public/js/profile.js
@@ -19,6 +19,21 @@ $(document).ready(function() {
     $("#achievementsAppend").text(data.achievements);
   });
 
+  $.get("/api/profile", function(data) {
+    console.log(data);
+    profileId = data.id;
+    $("#usernameAppend").text(data.username);
+    $("#locationAppend").text(data.location);
+    $("#nowPlayingAppend").text(data.topgamesPlayed);
+    $("#gameAppend01").text(data.game1);
+    $("#gameAppend02").text(data.game2);
+    $("#gameAppend03").text(data.game3);
+    $("#gameAppend04").text(data.game4);
+    $("#gameAppend05").text(data.game5);
+    $("#genreAppend").text(data.genres);
+    $("#aboutMeAppend").text(data.aboutme);
+    $("#achievementsAppend").text(data.achievements);
+  });
   // Delete the user profile that is displayed :
   $(".deleteBtn").on("click", function (event) {
     event.preventDefault();
